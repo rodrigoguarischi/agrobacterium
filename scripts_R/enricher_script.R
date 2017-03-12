94b42283b9d0960d4bd1df63d1a82babcba4566c
jholkid <jhonatasmonteiro@usp.br>
2017-03-12 19:00:37 -0300

    Adicionando loop para leitura dos arquivos

--- scripts_R/enricher_script.R
+++ scripts_R/enricher_script.R
@@ -1,5 +1,7 @@
 #clusterProfiler (version 3.0.5)
 
+options(stringsAsFactors = FALSE)
+
 #Instalando clusterProfiler
 
 source("https://bioconductor.org/biocLite.R")
@@ -9,20 +11,42 @@ require(clusterProfiler)
 
 #Carregando anotações com COG
 
-protein.id_cog <- read.delim("/mnt/work1/agrobacterium/jhonatas/cdd2cog/cdd2cog/results.bkp/protein-id_cog.txt", header=FALSE)
-cog_stats <- read.delim("/mnt/work1/agrobacterium/jhonatas/cdd2cog/cdd2cog/results.bkp/cog_stats.txt", header=FALSE)
-cog2gene = protein.id_cog[,c("V2","V1")]
+protein.id_cog <- read.delim( 
+  file = "/mnt/work1/agrobacterium/jhonatas/annotation/cdd2cog/cdd2cog/results.bkp/protein-id_cog.txt", 
+  header=FALSE, 
+  sep = "\t",
+  col.names = 1:max( count.fields("/mnt/work1/agrobacterium/jhonatas/annotation/cdd2cog/cdd2cog/results.bkp/protein-id_cog.txt", sep = "\t") )
+  );
+
+cog_stats <- read.delim("/mnt/work1/agrobacterium/jhonatas/annotation/cdd2cog/cdd2cog/results.bkp/cog_stats.txt", header=FALSE)
+
+cog2gene = protein.id_cog[,c(2,1)]
 cog2name = cog_stats[,c("V1","V2")]
+
+#Carregando anotações com KEGG
+
+kegg2gene <- read.delim("/mnt/work1/agrobacterium/jhonatas/annotation/kegg/results/kegg2gene.txt")
+kegg2name <- read.delim("/mnt/work1/agrobacterium/jhonatas/annotation/kegg/results/kegg2name.txt")
 
 #Carregando lista de genes diferencialmente expressos
 
-deg = as.vector(list_genes$V1)
+for( file in list.files(path = "new", pattern = "new_*", full.names = T) ){
+  deg = rownames(read.delim( file = file, header = T))
 
-#Executando o pacote clusterProfiler
+  #Executando o pacote clusterProfiler para cada "file"
+  x = enricher( deg, TERM2GENE = cog2gene, TERM2NAME = cog2name, minGSSize = 2, pAdjustMethod = "none")
+  
+  #Apresentando os resultados
+  
+  print( paste0("Arquivo = " , file) );
 
-x = enricher(deg, TERM2GENE = cog2gene, TERM2NAME = cog2name, qvalueCutoff = 0.05 )
+  print( table( deg %in% as.character( cog2gene[,2] ) ) )
+    
+  print( summary(x) )
+  
+  print( " " )
+  print( " " )
+  print( " " )
 
-#Apresentando os resultados
+}
 
-summary(x)
-barplot(x)
